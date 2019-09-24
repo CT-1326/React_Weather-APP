3cb9880c0475e81ae386d4361e92c2d99b5b9d9e
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2019-09-24 23:29:46 +0900

    Add location and permission

--- React_Weather-APP/App.js
+++ React_Weather-APP/App.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import Loading from './Loading'
+import {Alert} from 'react-native';
+import * as Location from 'expo-location';
 
-export default function App() {
-  return <Loading />;
+export default class extends React.Component{
+  state ={
+    isLoading : true
+  };
+
+  getLocation = async() =>{
+    try {
+      await Location.requestPermissionsAsync();
+      const{
+        coords:{lat,long}
+      }=await Location.getCurrentPositionAsync();
+      this.setState({isLoading: false});
+      console.log(coords.lat,coords.long);
+    } catch (e) {
+      Alert.alert("Can't find you!","so FFFFFuck");
+    }
+  }
+
+  componentDidMount(){
+    this.getLocation();
+  }
+
+  render(){
+    return <Loading />;
+  }
 }
